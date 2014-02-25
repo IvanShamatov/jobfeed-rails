807ccf3258ce555bce4cc8d5527510f53363268d
Ivan Shamatov <status.enable@gmail.com>
2014-02-25 15:11:10 +0400

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+JobfeedRails::Application.config.secret_key_base = '0b61c3c97742a396b5ff7abd6f43d2b24f3599ef31b4847f616b2decefcc95d7ee232e906bc0261869cceb950a55f3642209f53b74da47455f8d4c486d207d87'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+JobfeedRails::Application.config.session_store :cookie_store, key: '_jobfeed-rails_session'
